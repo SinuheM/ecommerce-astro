b16bff306a352ccefd04a779085f00dde887cd29
Adrian Priale <sinuhe-10@hotmail.com>
2024-08-31 03:03:14 -0500

    pruebita

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dounut-astro.vercel.app",
   integrations: [tailwind(), react()],
-  output: 'static',
-  adapter: vercelStatic(),
 });
