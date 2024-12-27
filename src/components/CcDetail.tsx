abe9c6745716472c7be50a5bd23a275b5d223d75
Adrian Priale <sinuhe-10@hotmail.com>
2024-12-26 23:48:38 -0500

    Mejora detalle

--- src/components/CcDetail.tsx
+++ src/components/CcDetail.tsx
@@ -19,9 +19,12 @@ const CcDetail = ({ productData }: { productData: CcProductItem }) => {
         <div className='md:ml-10 ml-5'>
           <ul className='text-gray-800 list-disc ml-4 md:mt-20'>
             <li className='md:text-xl text-lg my-3'>Precio: {' '}
-              <span className='line-through text-base'>S/. {productData.initialPrice}</span>
+              <span className='line-through text-base mr-1'>S/. {productData.initialPrice}.00</span>
               {' '}
-              <span className='text-green-800 text-2xl'>S/. {productData.price}</span>
+              <span className='text-teal-600 font-semibold text-2xl'>S/. {productData.price}.00</span>
+              {
+                productData.stock === 1 ? null : <span className="text-sm text-teal-600 font-semibold"> x unid.</span>
+              }
             </li>
             <li className='md:text-xl text-lg my-3'>Marca: {productData.brand}</li>
             <li className='md:text-xl text-lg my-3'>Categoría: {productData.category}</li>
